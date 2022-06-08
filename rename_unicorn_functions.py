5aae7cc34c09e0a0a8db89aa90fc1b589ec17f11
catatonicprime <catatonicprime@gmail.com>
2022-06-08 14:34:20 -0700

    almost right, close enough

--- rename_unicorn_functions.py
+++ rename_unicorn_functions.py
@@ -2,6 +2,7 @@
 # 
 import ghidra.app.decompiler as decomp
 from ghidra.program.model.symbol import FlowType
+from ghidra.program.model.symbol import SourceType
 
 
 # TODO: It'd be nice to identify the logv function here to populate this address auto-magically.
@@ -39,36 +40,35 @@ def getArgumentsForCaller(call):
   instr = target
   while True:
     instr = instr.previous
+
+    # If we've scanned out of the containing function and didn't find sufficient data to successfully analyze.
     if instr.address < parent.entryPoint:
-      # This means we didn't find sufficient data.
       return None
 
-    # We need to figure out how to handle MOV instructions
-    if instr.getMnemonicString() in ['MOV'] and instr.getRegister(0) and instr.getRegister(0).getName() in dest_registers:
+    # We need to figure out how to handle MOV instructions or other side-effects.
+    if instr.getMnemonicString() in ['MOV'] and instr.getRegister(0) and instr.getRegister(0).getName() in register_name_map:
       return None
 
-    # We're targeting LEA instructions to resolve their scalars
+    # We're targeting LEA instructions to resolve their scalars for their (probably) string data
     if instr.getMnemonicString() not in ['LEA']:
       continue
 
-    # Get the register used by the
+    # Get the destination register
     reg = instr.getRegister(0)
     if reg is None:
       continue
     if reg.getName() not in dest_registers:
       continue
     
-    try:
       input_objs = instr.getInputObjects()[0]
+    if input_objs is None:
+      return None
       data_addr = toAddr(input_objs.getValue())
       data = getDataAt(data_addr)
-    except:
-      print("Failed on input objects at instruction {} for call at {}".format(instr.address, call.fromAddress))
-      exit()
     name = "" if data is None else data.getValue()
-    if reg.getName() == 'RSI':
+    if reg.getName() == 'RSI' and funcname is None:
       funcname = name
-    if reg.getName() == 'RDI':
+    if reg.getName() == 'RDI' and filename is None:
       filename = name
 
     if filename is not None and funcname is not None:
@@ -81,6 +81,16 @@ for caller in callers:
   if args is None:
     continue
   renames.append(args)
-  print("{}, {}, {}, {}".format(caller.fromAddress, args[0] or "_", args[1] or "_func_{}".format(args[2]), args[3].fromAddress))
 
 print ("Found {} total functions to rename.".format(len(renames)))
+
+def mangle_name(filename, funcname):
+  return "{}/{}".format(filename.lstrip("../"), funcname)
+
+for rename in renames:
+  new_name = mangle_name(rename[0] or "_", rename[1] or "_func_{}".format(rename[2]))
+  fmt = "} | ".join("{{{{{")+"}"
+  print(fmt.format(rename[2], rename[0] or "_", rename[1] or "_func_{}".format(rename[2]), rename[3].fromAddress, new_name))
+  #symbol = getFunctionAt(rename[2])
+  #symbol.setName(new_name, SourceType.USER_DEFINED)
+  #print(symbol)
