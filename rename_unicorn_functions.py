cdbbaf4dbb810d0558bd6c41f5f3ac1ea5771ab0
catatonicprime <catatonicprime@gmail.com>
2022-06-08 22:36:15 -0700

    genericized the call argument parser a little bit.

--- rename_unicorn_functions.py
+++ rename_unicorn_functions.py
@@ -4,37 +4,21 @@ import ghidra.app.decompiler as decomp
 from ghidra.program.model.symbol import FlowType
 from ghidra.program.model.symbol import SourceType
 
+def mangle_name(filename, funcname):
+  return "{}/{}".format(filename.lstrip("../"), funcname)
 
-# TODO: It'd be nice to identify the logv function here to populate this address auto-magically.
-
-logv_addr = toAddr(0x02d1b8d0)
-logv = getFunctionContaining(logv_addr)
-logv_entry = logv.getEntryPoint()
-
-print("Renaming based on references to {}: {}".format(logv_entry, logv))
-
-# From the starting spot, find the calling locations.
-
-callers = [caller for caller in getReferencesTo(logv_entry) 
-           if caller.getReferenceType() == FlowType.UNCONDITIONAL_CALL
-             and getFunctionContaining(caller.fromAddress)]
-
-orphan_callers = [caller for caller in getReferencesTo(logv_entry) 
-           if caller.getReferenceType() == FlowType.UNCONDITIONAL_CALL
-             and getFunctionContaining(caller.fromAddress) is None]
-
-orphan_callers.sort()
-callers.sort()
-
-
-def getArgumentsForCaller(call):
+def getArgumentsForCall(call, register_name_map={}):
   # call: location to attempt to discover arguments for.
-  # 
+  # register_name_map: map of registers to a more semantic name, e.g. {"RSI": "function", "RDI": "file"}
   # RSI -> function name
   # RDI -> file name
-  dest_registers = ['RSI','RDI']
-  filename = None
-  funcname = None
+  if len(register_name_map) == 0:
+    raise Exception("register_name_map needs to have at least one {'register': 'name'} pair")
+  
+  name_value_map = {}
+  for name in register_name_map.values():
+    name_value_map[name] = None
+
   parent = getFunctionContaining(call.fromAddress)
   target = currentProgram.getListing().getInstructionAt(call.fromAddress)
   instr = target
@@ -57,40 +41,65 @@ def getArgumentsForCaller(call):
     reg = instr.getRegister(0)
     if reg is None:
       continue
-    if reg.getName() not in dest_registers:
+    
+    # Is this one of the targeted regisers to resolve?
+    if reg.getName() not in register_name_map:
+      continue
+
+    # If this result has already been found then continue
+    value_name = register_name_map[reg.getName()]
+    if name_value_map[value_name] is not None:
       continue
     
-      input_objs = instr.getInputObjects()[0]
+    # Recover the scalar or bail because we must have missed the copy or it was dynamic
+    input_objs = instr.getInputObjects()[0]
     if input_objs is None:
       return None
-      data_addr = toAddr(input_objs.getValue())
-      data = getDataAt(data_addr)
-    name = "" if data is None else data.getValue()
-    if reg.getName() == 'RSI' and funcname is None:
-      funcname = name
-    if reg.getName() == 'RDI' and filename is None:
-      filename = name
+    data_addr = toAddr(input_objs.getValue())
+    data = getDataAt(data_addr)
+    resolved_data = "" if data is None else data.getValue()
 
-    if filename is not None and funcname is not None:
+    name_value_map[value_name] = resolved_data
+
+    # if there are no remaining values to map (all of them are set) then we're done
+    if len([key for key in name_value_map.keys() if name_value_map[key] is None]) == 0:
       break
-  return (filename, funcname, parent.entryPoint, call)
+  return (name_value_map, parent.entryPoint, call)
+
+# TODO: It'd be nice to identify the logv function here to populate this address auto-magically.
+
+logv = getFunctionContaining(askAddress('','wheres the verbose log function?'))
+logv_entry = logv.getEntryPoint()
+
+# From the starting spot, find the calling locations.
+
+callers = [caller for caller in getReferencesTo(logv_entry) 
+           if caller.getReferenceType() == FlowType.UNCONDITIONAL_CALL
+             and getFunctionContaining(caller.fromAddress)]
+
+orphan_callers = [caller for caller in getReferencesTo(logv_entry) 
+           if caller.getReferenceType() == FlowType.UNCONDITIONAL_CALL
+             and getFunctionContaining(caller.fromAddress) is None]
+
+orphan_callers.sort()
+callers.sort()
+
 
 renames = []
 for caller in callers:
-  args = getArgumentsForCaller(caller)
+  args = getArgumentsForCall(caller, {'RSI': 'function', 'RDI': 'file'})
   if args is None:
     continue
   renames.append(args)
 
 print ("Found {} total functions to rename.".format(len(renames)))
 
-def mangle_name(filename, funcname):
-  return "{}/{}".format(filename.lstrip("../"), funcname)
-
 for rename in renames:
-  new_name = mangle_name(rename[0] or "_", rename[1] or "_func_{}".format(rename[2]))
-  fmt = "} | ".join("{{{{{")+"}"
-  print(fmt.format(rename[2], rename[0] or "_", rename[1] or "_func_{}".format(rename[2]), rename[3].fromAddress, new_name))
-  #symbol = getFunctionAt(rename[2])
+  functionname = rename[0]['function'] if rename[0] else None
+  filename = rename[0]['file'] if rename[0] else None
+  new_name = mangle_name(filename or "_", functionname or "_func_{}".format(rename[1]))
+  print("{} -> {}".format(rename, new_name))
+  symbol = getFunctionAt(rename[2])
+  print(symbol)
   #symbol.setName(new_name, SourceType.USER_DEFINED)
-  #print(symbol)
+  
