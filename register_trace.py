b0ce3b9fd90f4e3dfbb4dfeecb4e07d46c7b92c8
catatonicprime <catatonicprime@gmail.com>
2024-08-29 08:11:10 -0700

    refactoring the order of checks to make more sense after some sleep

--- register_trace.py
+++ register_trace.py
@@ -37,39 +37,42 @@ def resolveData(address, resolveType='address'):
 def traceRegisters(call, target_registers):
   # We start at the called from address and then work toward previous instructions.
   address = call.fromAddress
-  instr = getInstructionAt(address).getPrevious()
+  instr = getInstructionAt(address)
   containing_function = getFunctionContaining(address).getEntryPoint()
 
+  # Don't leave the containing function... if we don't have an answer by then, bail.
   while(address >= containing_function):
-    # print("--- ResultObjects")
-    result_objects = instr.getResultObjects()
-    if len(result_objects) < 1:
-      print("*** Unexpected...  ResultObjects was empty")
-      continue
-    # print(result_objects[0])
-
-    if any(str(item) in target_registers for item in result_objects):
-      input_objs = instr.getInputObjects()[0]
-      if input_objs is None:
-        print("*** Unexpected...  getInputObjects was empty")
-        break
-      if isinstance(input_objs, ghidra.program.model.lang.Register):    
-        return None
-      print(instr.getAddress())
-      print("--- Source Data: {}".format(resolveData(input_objs.getValue())))
-      break
-    
-    instr = instr.getPrevious()
-    if not instr:
-      break
     # non-Fallthroughs can look like inter-procedural calls, i.e. a call to another functiont that might affect
     # a register... but tracking into them sounds like a huge pain in the ass & probably not necesary in 
     # simpler use-cases that can still be very useful!
     # Luckily a non-fall through appear to be instructions *after* a call
+    instr = instr.getPrevious()
+    if not instr:
+      break
     if not instr.isFallthrough():
       break
-    # print("--- Previous instruction")
+    # print("--- ResultObjects")
+    result_objects = instr.getResultObjects()
+    if len(result_objects) < 1:
+      # Ignore result objects with no results (nop?)
+      print("*** Unexpected...  ResultObjects was empty")
+      continue
 
+    # Quick n dirty check if the result objects intersect with any of our target registers
+    # Note: we're only looking for full registers here, partial registers would
+    # need some more consideration.
+    if not any(str(item) in target_registers for item in result_objects):
+      continue
+
+    input_objs = instr.getInputObjects()[0]
+    if input_objs is None:
+      print("*** Unexpected...  getInputObjects was empty")
+      break
+    if isinstance(input_objs, ghidra.program.model.lang.Register):    
+      return None
+    print(instr.getAddress())
+    print("--- Source Data: {}".format(resolveData(input_objs.getValue())))
+    break
 
 data_stack = []
 
@@ -87,10 +90,6 @@ lua_pushcclosure_calls_with_containing_function.sort()
 # lua_pushcclosure_calls_with_containing_function = lua_pushcclosure_calls_with_containing_function[:1]
 
 # Now we have all the calls we want to investigate.
-# These represent the last instruction in a series of instructions which should terminate:
-# 1. At the start of the containing function
-# 2. At the first write to the target register or smaller variations.
-# 3. At the first complete write to the target register.
 
 print("--- Calls")
 for call in lua_pushcclosure_calls_with_containing_function:
